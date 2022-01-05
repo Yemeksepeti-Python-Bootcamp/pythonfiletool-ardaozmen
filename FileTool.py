3975829289c89fa2a388f0ea88e1ad244455aa74
Arda Cem Özmen <ardaozmen3@gmail.com>
2022-01-05 10:00:01 +0300

    mergeFiles function has been added.

--- FileTool.py
+++ FileTool.py
@@ -1,13 +1,14 @@
-import csv
+import csv,glob
 import os.path
 import pandas as pd
 from csv import writer
 
 class FileTool:
-    def __init__(self, path, fields = [], *args, **kwargs):
+    
+    def __init__(self, path, fields = [], *args):
         self.path = path
         self.fields = fields
-
+        
     def isFileExist(self):
         '''
         - checks the file path. 
@@ -20,14 +21,13 @@ class FileTool:
         - If there is no file matching the format in the path, 
         - it creates a new file.
         '''
-        isFile = isFileExist(self.path)
+        isFile = self.isFileExist()
         if isFile == True:
             print("No need to create new file! File is exist.")
         elif isFile == False:
             with open(path, 'w', encoding='utf-8') as file:
                 writer = csv.writer(file)
-                fields = [item for item in input("Enter the list of fields: ").split()]
-                writer.writerow(fields)
+                writer.writerow(self.fields)
 
     def toJson(self):
         '''
@@ -35,11 +35,11 @@ class FileTool:
         '''
         with open(self.path, 'r') as file_csv:
             reader = csv.DictReader(file_csv)
-        with open('myfile.json', 'w') as file_json: 
+        with open('{}'.format(path.split('.')[0]+'.json'), 'w') as file_json:
             for row in reader:
                 json.dump(row, file_json)
 
-    def searchData(self): 
+    def searchData(self):
         '''
         - Prints the data containing the searched word.
         '''
@@ -54,9 +54,9 @@ class FileTool:
         - Adds new data to the data in a new row.
         '''
         appended_ = [item for item in input("Enter the data with spaces: ").split()]
-        with open(self.path, 'a', newline='') as f_object:  
+        with open(self.path, 'a', newline='') as f_object:
             writer_object = writer(f_object)
-            writer_object.writerow(appended_)  
+            writer_object.writerow2(appended_)  
             f_object.close()
 
     def deleteData(self):
@@ -78,6 +78,22 @@ class FileTool:
         df.replace('{}'.format(input("Enter a old value: ")),'{}'.format(input("Enter a new value: ")), inplace=True)
         return df
 
+    def mergeFiles(self): # TODO some bugs will be fixed.
+        '''
+        - Merges JSON or CSV all files in path.
+        '''
+        if self.path.endswith(".csv"): 
+            dfs = glob.glob('path/*.csv')
+            result = pd.concat([pd.read_csv(df) for df in dfs], ignore_index=True)
+            result.to_csv('path/merged.csv', ignore_index=True)
+        elif self.path.endswith(".json"): 
+            read_files = glob.glob("*.json")
+            with open("merged_file.json", "wb") as outfile:
+                outfile.write('[{}]'.format(
+                    ','.join([open(f, "rb").read() for f in read_files])))
+        else:
+            print('The file format is not suitable.')
+    
     def Menu(self):
         '''
         Press [1] to search data,
@@ -86,10 +102,11 @@ class FileTool:
         Press [4] to update data,
         Press [5] to create new file,
         Press [6] to convert csv2json format,
-        Press [7] to quit the program,
+        Press [7] to merge files,
+        Press [8] to quit the program,
         '''
         while True:
-            select_ = input("Press [1] to search data,\nPress [2] to append data,\nPress [3] to delete data,\nPress [4] to update data,\nPress [5] to create new file,\nPress [6] to convert csv2json format,\nPress [7] to quit the program,\nSelect the action you want to do: ")
+            select_ = input("Press [1] to search data,\nPress [2] to append data,\nPress [3] to delete data,\nPress [4] to update data,\nPress [5] to create new file,\nPress [6] to convert csv2json format,\nPress [7] to merge files,\nPress [8] to quit the program,\nSelect the action you want to do: ")
             if select_ == '1':
                 self.searchData()
             elif select_ == '2':
@@ -105,6 +122,6 @@ class FileTool:
             elif select_ == '7':
                 break
 
-path = 'innovators.csv'
-ft = FileTool(path)
+path = 'deneme2.csv'
+ft = FileTool(path, fields=['Name','Contribution'])
 ft.Menu()
